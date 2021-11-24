9adfec4a18d6f0d81e4f9b00b4d718d121614473
Igor Chakrygin <igor@chakrygin.ru>
2021-11-24 23:39:02 +0300

    dev

--- scripts/dev-up.cmd
+++ scripts/dev-up.cmd
@@ -4,4 +4,28 @@ pushd "%~dp0.."
 
 vagrant up
 
+
+if errorlevel 1 (
+    exit 1
+)
+
+if not exist ".kube\config" (
+    echo File ".kube\config" not found!
+)
+
+if not exist "%UserProfile%\.kube\" (
+    mkdir "%UserProfile%\.kube\"
+)
+
+echo Copying ".kube\config" to "%UserProfile%\.kube\config"
+copy ".kube\config" "%UserProfile%\.kube\config"
+
+if not exist "%UserProfile%\YandexDisk\.kube\" (
+    mkdir "%UserProfile%\YandexDisk\.kube\"
+)
+
+echo Copying ".kube\config" to "%UserProfile%\YandexDisk\.kube\config"
+copy ".kube\config" "%UserProfile%\YandexDisk\.kube\config"
+
+
 popd
