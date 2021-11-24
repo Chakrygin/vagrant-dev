a42fdbf9a01b68cb5eac0e12ac7afd19353cd7e1
Igor Chakrygin <igor@chakrygin.ru>
2021-11-24 23:07:29 +0300

    dev

--- provision.sh
+++ provision.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+echo
+echo "############################################################"
+echo "## Installing utilities...                                ##"
+echo "############################################################"
+
+apt-get --yes update
+apt-get --yes install mc
