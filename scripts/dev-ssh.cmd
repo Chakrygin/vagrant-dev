8b6d2953783e82df11bc7701b315729d6ceb246d
Igor Chakrygin <igor@chakrygin.ru>
2021-11-24 23:22:05 +0300

    dev

--- scripts/dev-ssh.cmd
+++ scripts/dev-ssh.cmd
@@ -1,3 +1,13 @@
 @echo off
 
-wt new-tab -d "%~dp0.." cmd /C vagrant ssh "node-1"
+@REM wt new-tab -d "%~dp0.." cmd /C vagrant ssh "node-1"
+
+ssh -q
+    -o UserKnownHostsFile=/dev/null \
+    -o StrictHostKeyChecking=no \
+    -i ~/.vagrant.d/insecure_private_key \
+    vagrant@localhost \
+    -p $PORT \
+    "$@"
+
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i .\.vagrant\machines\node-1\virtualbox\private_key vagrant@192.168.1.101
