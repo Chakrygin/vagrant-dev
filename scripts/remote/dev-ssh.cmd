e783f1ce026467561bb08e996feceab2c9166168
Igor Chakrygin <igor@chakrygin.ru>
2021-11-25 01:04:11 +0300

    dev

--- scripts/remote/dev-ssh.cmd
+++ scripts/remote/dev-ssh.cmd
@@ -4,12 +4,11 @@ pushd "%~dp0.."
 
 set PRIVATE_KEY_PATH=%UserProfile%\YandexDisk\.dev\node-1\private_key
 
-wt new-tab -d "%~dp0.." cmd /C ssh ^
+start ssh ^
     -q ^
     -o "UserKnownHostsFile=/dev/null" ^
     -o "StrictHostKeyChecking=no" ^
     -i "%PRIVATE_KEY_PATH%" ^
-    "vagrant@192.168.1.101" ^
-    %*
+    "vagrant@192.168.1.101"
 
 popd
