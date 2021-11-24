8cb988cde13c586089e8ffa2a0506c56e9f99ab1
Igor Chakrygin <igor@chakrygin.ru>
2021-11-24 23:39:06 +0300

    dev

--- scripts/dev-ssh.cmd
+++ scripts/dev-ssh.cmd
@@ -1,13 +1,51 @@
 @echo off
 
-@REM wt new-tab -d "%~dp0.." cmd /C vagrant ssh "node-1"
+pushd "%~dp0.."
 
-ssh -q
-    -o UserKnownHostsFile=/dev/null \
-    -o StrictHostKeyChecking=no \
-    -i ~/.vagrant.d/insecure_private_key \
-    vagrant@localhost \
-    -p $PORT \
-    "$@"
+for %%a in (
+    ".vagrant\machines\node-1\virtualbox\private_key"
+    "%UserProfile%\YandexDisk\.dev\private_key"
+) do (
+    if "%PRIVATE_KEY_PATH%" == "" (
+        if exist "%%~a" (
+            echo %%~a
+        )
+    )
+)
 
-ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i .\.vagrant\machines\node-1\virtualbox\private_key vagrant@192.168.1.101
+if not exist "%PRIVATE_KEY_PATH%" (
+    echo ERROR
+    exit 1
+)
+
+@REM If "%%~aZ" GEq "d" (
+@REM         GoTo DIR
+@REM     ) Else (
+@REM         If "%%~aZ" GEq "-" (
+@REM             GoTo FILE
+@REM         ) Else (
+@REM             GoTo NOACCESS
+@REM         )
+@REM     )
+
+@REM wt new-tab -d "%~dp0.." cmd /C ssh ^
+@REM     -q ^
+@REM     -o "UserKnownHostsFile=/dev/null" ^
+@REM     -o "StrictHostKeyChecking=no" ^
+@REM     -i .\.vagrant\machines\node-1\virtualbox\private_key ^
+@REM     "vagrant@192.168.1.101"
+
+
+@REM @REM wt new-tab -d "%~dp0.." cmd /C vagrant ssh "node-1"
+
+@REM ssh -q
+@REM     -o UserKnownHostsFile=/dev/null \
+@REM     -o StrictHostKeyChecking=no \
+@REM     -i ~/.vagrant.d/insecure_private_key \
+@REM     vagrant@localhost \
+@REM     -p $PORT \
+@REM     "$@"
+
+@REM ssh
+
+popd
